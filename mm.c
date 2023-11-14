bf2276acb6f771dfe07abd7d0531a08e3531e35a
Ubuntu <ubuntu@ip-172-31-41-4.ap-northeast-2.compute.internal>
2023-11-14 13:09:22 +0000

    explicit debgging중

--- mm.c
+++ mm.c
@@ -74,6 +74,7 @@ static void *find_fit(size_t);
 static void *first_fit(size_t);
 static void *next_fit(size_t);
 static void place(void *, size_t);
+static void *explicit_fit(size_t);
 static void add_explicit_free_block(char *);
 static void splice_explicit_free_block(char *);
 static int mm_check(void);
@@ -86,16 +87,43 @@ static char *free_list_head;
  */
 int mm_init(void)
 {
-    if ((heap_listp=mem_sbrk(4*WSIZE)) == (void *)-1)
+    if ((heap_listp=mem_sbrk(12*WSIZE)) == (void *)-1)
         return -1;
     PUT(heap_listp, 0);
-    PUT(heap_listp+(1*WSIZE), PACK(DSIZE, 1));
-    PUT(heap_listp+(2*WSIZE), PACK(DSIZE, 1));
-    PUT(heap_listp+(3*WSIZE), PACK(0, 1));
-    heap_listp+=(2*WSIZE);
-    next_fit_ptr = heap_listp;
+
+    //free head
+    PUT(heap_listp+(1*WSIZE),PACK(DSIZE*2,1));
+    PUT(heap_listp+(2*WSIZE), 0);
+    PUT(heap_listp+(3*WSIZE), 0);
+    PUT(heap_listp+(4*WSIZE),PACK(DSIZE*2,1));
+
+    //free tail
+    PUT(heap_listp+(5*WSIZE),PACK(DSIZE*2,1));
+    PUT(heap_listp+(6*WSIZE), 0);
+    PUT(heap_listp+(7*WSIZE), 0);
+    PUT(heap_listp+(8*WSIZE),PACK(DSIZE*2,1));
+
+    //prologue
+    PUT(heap_listp+(9*WSIZE), PACK(DSIZE, 1)); //header
+    PUT(heap_listp+(10*WSIZE), PACK(DSIZE, 1)); //footer
+    PUT(heap_listp+(11*WSIZE), PACK(0, 1)); //eplilogue
+    heap_listp+=(10*WSIZE);
+    
     if (extend_heap(CHUNKSIZE/WSIZE)==NULL)
         return -1;
+
+    char *free_head_bp = heap_listp+(2*WSIZE);
+    char *free_tail_bp = heap_listp+(6*WSIZE);
+    free_list_head = free_head_bp;
+    //free_head 앞 뒤 세팅
+    PUT(free_list_head, free_head_bp);// free_list_head가 free_head 가리키게
+    PUT(PRED(free_head_bp), NULL); // head의 앞. null
+    PUT(SUCC(free_head_bp), free_tail_bp); //head뒤
+    
+    //free_tail 앞 뒤 세팅
+    PUT(PRED(free_tail_bp), free_head_bp); // tail 의 앞
+    PUT(SUCC(free_tail_bp), NULL); // tail의 뒤
+
     return 0;
 }
 
@@ -194,16 +222,17 @@ static void *extend_heap(size_t words){
     return coalesce(bp);
 }
 
-void add_explicit_free_block(char * bp){
+void add_explicit_free_block(char * bp){ //LIFO
 
-    char *first_node = GET(free_list_head); // 연결 리스트(explicit free list)의 첫 번째 노드
+    char *head_node = GET(free_list_head); // 연결 리스트(explicit free list)의 헤드
+    char *first_node = GET(SUCC(head_node)); // 연결 리스트의 first node;
+    // first_node 와 bp의 관계
+    PUT(SUCC(head_node), bp);// head의 successor 에 bp 넣기
+    PUT(PRED(bp), head_node); // bp의 predecessor에 head 넣기
 
-    // head 와 bp의 관계
-    PUT(free_list_head, bp);// head의 successor 에 bp 넣기
-    PUT(PRED(bp), free_list_head); // bp의 predecessor에 head 주소 넣기
-    // bp 와 first_node의 관계
+    // bp 와 다음 노드의 관계
     PUT(SUCC(bp), first_node); // bp의 successor에 first_node 넣기
-    PUT(PRED(first_node), bp); // first_node의 predecessor에 bp 포인터 넣기
+    PUT(PRED(first_node), bp); // first_node의 predecessor에 bp 넣기
 
 }
 
@@ -276,8 +305,26 @@ static void *coalesce(void *bp){
 
 static void *find_fit(size_t asize)
 {
+    return explicit_fit(asize);
     // return first_fit(asize);
-    return next_fit(asize);
+    //return next_fit(asize);
+}
+
+static void* explicit_fit(size_t asize){
+    
+    size_t size;
+
+    char *bp = free_list_head;
+
+    while(SUCC(bp)!=NULL){
+
+        size = GET_SIZE(HDRP(bp));
+        
+        if (size >= asize)
+            return bp;
+        bp = SUCC(bp);
+    }
+    return NULL;
 
 }
 
@@ -336,30 +383,18 @@ static void* next_fit(size_t asize)
 
 static void place(void *bp, size_t asize){
     size_t base_size = GET_SIZE(HDRP(bp));
-    // printf("할당 중 %p \n할당size: %d 원래size:%d\n", bp, asize, base_size);
     // 남은 free block이 4 words 미만일 때
     if((base_size - asize) < 4 * WSIZE){
-        // printf("내부단편화로 할당\n");
-        // 남은 free block까지 포함해서 내부적 단편화(internal fragmentation)하기
         PUT(FTRP(bp), PACK(base_size, 1));
         PUT(HDRP(bp), PACK(base_size, 1));
     }
     // 남은 free block이 4 words 이상일 때
-    else{
-        // printf("새 블록 쪼개면서 할당\n");
-        // allocated block
+    else{  //split
         PUT(HDRP(bp), PACK(asize, 1));
         PUT(FTRP(bp), PACK(asize, 1));
-        // printf("앞 hdr:%p size%d ftr%p\n",
-        //     HDRP(bp), GET_SIZE(HDRP(bp)), FTRP(bp)
-        // );
-        // free block
-        PUT(HDRP(NEXT_BLKP(bp)), PACK(base_size-asize, 0));
+        PUT(HDRP(NEXT_BLKP(bp)), PACK(base_size-asize, 0)); //암시적으로 넥스트로 감
         PUT(FTRP(NEXT_BLKP(bp)), PACK(base_size-asize, 0));
-        // printf("뒤 hdr:%p size%d ftr%p\n",
-        //     HDRP(NEXT_BLKP(bp)), GET_SIZE(HDRP(NEXT_BLKP(bp))), 
-        //     FTRP(NEXT_BLKP(bp))
-        // );
+        add_explicit_free_block(NEXT_BLKP(bp));
     }
     next_fit_ptr = NEXT_BLKP(bp);
     return;
